b2750b0c6181064b7f7aa371818adedd4c9951be
김호준 <rlaghwns126@naver.com>
2022-03-10 15:49:41 +0900

    이미지변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import black from './black.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={black} className="App-logo" alt="logo" />
         <p>
           한글로 바꾸어 보아요 <code>src/App.js</code> and save to reload.
         </p>
